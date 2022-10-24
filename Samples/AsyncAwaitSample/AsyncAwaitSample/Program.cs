9fd9ac60d860ad54fb87a514c0e09832a11b8dd0
Mofaggol Hoshen <mofaggol.hoshen@quipu.de>
2022-10-24 08:29:48 +0200

    AsyncAwait sample added

---
+++ Samples/AsyncAwaitSample/AsyncAwaitSample/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var person = new Person
+        {
+            Id = 10,
+            Name = "Mofaggol Hoshen",
+            Email = "mofaggol.hoshen@gmail.com"
+        };
+
+        person.OnEmailChanged += async (email) =>
+        {
+
+            Console.WriteLine("First subscription.");
+
+            var i = await GetTaskAsync();
+
+            Console.WriteLine($"First subscription: {1+10}");
+        };
+
+
+        person.OnEmailChanged += async (email) =>
+
+        {
+            Console.WriteLine("Second subscription.");
+
+            var i = await GetTaskAsync();
+
+            Console.WriteLine($"Second subscription: {1+20}");
+        };
+
+        await person.Update(new Person
+        {
+            Id=12,
+            Name="Hoshen",
+            Email="mofaggol.hoshen@outlook.com"
+        });
+
+        Console.WriteLine("Finished");
+
+    }
+
+    public static async Task<int> GetTaskAsync()
+    {
+        Console.WriteLine($"GetTaskAsync function.");
+
+        return await HttpRequest();
+    }
+
+    public static async Task<int> HttpRequest()
+    {
+        await Task.Delay(500);
+
+        Console.WriteLine($"HttpRequest function.");
+
+        return 10;
+    }
+}
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public Func<string, Task> OnEmailChanged;
+
+    public async Task Update(Person person)
+    {
+        this.Id = person.Id;
+
+        if (person.Name != this.Name)
+            this.Name = person.Name;
+
+        if (person.Email != this.Email)
+        {
+            await OnEmailChanged.Invoke(person.Email);
+            this.Email = person.Email;
+
+        }
+    }
+}
---
+++ Samples/BenchmardExercise/BenchmardExercise/ListForEachVsForLoop.cs
@@ -0,0 +1,79 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BenchmardExercise;
+
+[MemoryDiagnoser]
+public  class ListForEachVsForLoop
+{
+    private List<string> _items;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _items = new List<string>(){
+            "Test",
+            "Test1",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2",
+            "Test2"
+        };
+
+
+    }
+
+    [Benchmark]
+    public void List_ForEach()
+    {
+        _items.ForEach(i => {
+        });
+    }
+
+    [Benchmark]
+    public void ForEach()
+    {
+        foreach (var str in _items)
+        {
+        }
+    }
+
+    [Benchmark]
+    public void For()
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+        }
+    }
+}
+
+/****************************Result**********************************************
+ * 
+ * 
+ * |       Method |      Mean |     Error |   StdDev |    Median | Allocated |
+ * |------------- |----------:|----------:|---------:|----------:|----------:|
+ * | List_ForEach | 42.746 ns | 1.8567 ns | 5.176 ns | 41.181 ns |         - |
+ * |      ForEach | 25.304 ns | 0.5654 ns | 1.557 ns | 24.945 ns |         - |
+ * |          For |  8.776 ns | 1.0132 ns | 2.987 ns |  7.655 ns |         - |
+ * 
+ *   Mean      : Arithmetic mean of all measurements
+ *   Error     : Half of 99.9% confidence interval
+ *   StdDev    : Standard deviation of all measurements
+ *   Median    : Value separating the higher half of all measurements (50th percentile)
+ *   Allocated : Allocated memory per single operation (managed only, inclusive, 1KB = 1024B)
+ *   1 ns      : 1 Nanosecond (0.000000001 sec)
+ * 
+ * 
+ *********************************************************************************/

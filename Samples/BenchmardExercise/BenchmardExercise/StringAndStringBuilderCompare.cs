fdeb945081b2c6d932d5980452cff2a118b7937c
mofaggol.hoshen@quipu.de <mofaggol.hoshen@quipu.de>
2023-05-24 12:13:43 +0200

    updated

--- Samples/BenchmardExercise/BenchmardExercise/StringAndStringBuilderCompare.cs
+++ Samples/BenchmardExercise/BenchmardExercise/StringAndStringBuilderCompare.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,24 +26,30 @@ public class StringAndStringBuilderCompare
     [Benchmark]
     public string StrConcat()
     {
+        //DefaultInterpolatedStringHandler defaultInterpolatedStringHandler = new DefaultInterpolatedStringHandler(6, 1);
+        //defaultInterpolatedStringHandler.AppendLiteral("Hello ");
+        //defaultInterpolatedStringHandler.AppendFormatted(10);
+        //string text = defaultInterpolatedStringHandler.ToStringAndClear();
+
         string str = "";
         for (int i = 0; i < 1000; i++)
         {
+            // it's using span<char>
             str = $"{i}";
         }
 
         return str;
     }
 
-    [Benchmark]
-    public void Str1()
-    {
+    //[Benchmark]
+    //public void Str1()
+    //{
 
-        for (int i = 0; i < 1000; i++)
-        {
-            StrAndStrBuidler person = new(i, i.ToString());
-        }
-    }
+    //    for (int i = 0; i < 1000; i++)
+    //    {
+    //        StrAndStrBuidler person = new(i, i.ToString());
+    //    }
+    //}
 
     [Benchmark]
     public string StrBuilder()
